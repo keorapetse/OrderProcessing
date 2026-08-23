54372f67df439f2eea1a90dd29bbc1b7223cd3dd
Keorapetse Fokane <keorapetsefokane55@gmail.com>
2026-08-23 16:59:54 +0200

    added interface classes for inventory and payments

---
+++ src/OrderProcessing.Api/Interfaces/IInventoryService.cs
@@ -0,0 +1,10 @@
+﻿using OrderProcessing.Api.Models;
+
+namespace OrderProcessing.Api.Interfaces
+{
+    public interface IInventoryService
+    {
+        Task<InventoryItem?> GetAvailableProductsAsync(string productId);
+        Task<InventoryItem?> ReserveStockAsync(string productId, int quantity);
+    }
+}
---
+++ src/OrderProcessing.Api/Interfaces/IPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using OrderProcessing.Api.Models;
+
+namespace OrderProcessing.Api.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<PaymentTransaction> ProcessPaymentAsync(Guid orderId, decimal amount);
+        Task<PaymentTransaction> GetPaymentStatusAsync(Guid transactionId);
+
+    }
+}
