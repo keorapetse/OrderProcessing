90a5f8846276f8d35fe60213747a4eea7231fcd7
Keorapetse Fokane <keorapetsefokane55@gmail.com>
2026-08-23 17:01:28 +0200

    created a service for processing a payment and checking the status of the payment

---
+++ src/OrderProcessing.Api/Services/PaymentService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderProcessing.Api.Data;
+using OrderProcessing.Api.Interfaces;
+using OrderProcessing.Api.Models;
+
+namespace OrderProcessing.Api.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly AppDbContext _db;
+        private readonly ILogger<PaymentService> _logger;
+
+        public PaymentService(AppDbContext appDbContext, ILogger<PaymentService> logger)
+        {
+            _db = appDbContext;
+            _logger = logger;
+        }
+
+        public async Task<PaymentTransaction> ProcessPaymentAsync(Guid orderId, decimal amount)
+        {
+            var paymentTransaction = new PaymentTransaction
+            {
+                TransactionId = Guid.NewGuid(),
+                OrderId = orderId,
+                Amount = amount,
+                Status = PaymentStatus.Completed,
+                ProcessedAt = DateTime.UtcNow
+            };
+
+            _db.PaymentTransactions.Add(paymentTransaction);
+            await _db.SaveChangesAsync();
+            return paymentTransaction;
+        }
+
+        public async Task<PaymentTransaction> GetPaymentStatusAsync(Guid transactionId)
+        {
+            var paymentTransaction = await _db.PaymentTransactions.FirstOrDefaultAsync(x => x.TransactionId == transactionId);
+            return paymentTransaction;
+        }
+    }
+}
