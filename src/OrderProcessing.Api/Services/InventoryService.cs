fa76f2f074b23030f58f85706974e7999da69fd5
Keorapetse Fokane <keorapetsefokane55@gmail.com>
2026-08-23 17:00:44 +0200

    created a service for checking available products and reserve products

---
+++ src/OrderProcessing.Api/Services/InventoryService.cs
@@ -0,0 +1,42 @@
+﻿using OrderProcessing.Api.Data;
+using OrderProcessing.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using OrderProcessing.Api.Interfaces;
+using static OrderProcessing.Api.Dtos.InventoryDto;
+
+namespace OrderProcessing.Api.Services
+{
+    public class InventoryService : IInventoryService
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly ILogger<InventoryService> _logger;
+
+        public InventoryService (AppDbContext appDbContext, ILogger<InventoryService> logger)
+        {
+            _appDbContext = appDbContext;
+            _logger = logger;
+        }
+
+        public async Task<InventoryItem?> GetAvailableProductsAsync(string productId)
+        {
+            var availableStock = await _appDbContext.InventoryItems.FirstOrDefaultAsync(i => i.ProductId == productId);
+            return availableStock;
+        }
+
+        //will come back to this later
+        public async Task<InventoryItem?> ReserveStockAsync(string product, int quantity)
+        {
+            var inventoryItem = await GetAvailableProductsAsync(product);
+
+            if (inventoryItem.AvailableQuantity < quantity)
+            {
+                _logger.LogWarning($"Cannot reserve {quantity} units for product ID: {product}. Available quantity: {inventoryItem.AvailableQuantity}");
+                return null;
+            }
+
+            await _appDbContext.SaveChangesAsync();
+
+            return inventoryItem;
+        }
+    }
+}
