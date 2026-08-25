91f7cd100f8d3e4a9824f62cf71f309d3ee84341
Keorapetse Fokane <keorapetsefokane55@gmail.com>
2026-08-25 18:49:48 +0200

    delete unneeded file

--- src/OrderProcessing.Api/Dtos/CreateOrderResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OrderProcessing.Api.Dtos
-{
-    public class CreateOrderResponse
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; } = string.Empty;
-        public OrderResponseDto? Order { get; set; }
-    }
-}
